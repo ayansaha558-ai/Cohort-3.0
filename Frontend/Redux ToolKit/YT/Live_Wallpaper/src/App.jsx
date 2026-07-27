73885a632343562e765cd5bb64baaa43693c78e9
ayansaha558-ai <ayan.saha558@gmail.com>
2026-07-27 18:03:49 +0530

    Redux Toolkit added

--- Frontend/Redux ToolKit/YT/Live_Wallpaper/src/App.jsx
+++ Frontend/Redux ToolKit/YT/Live_Wallpaper/src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import NavBar from './components/Navbar'
-import SearchBar from './components/SearchBar'
-import Tabs from "./components/Tabs"
-import ResultGrid from './components/ResultGrid'
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import Tabs from "./components/Tabs";
+import ResultGrid from "./components/ResultGrid";
 
 const App = () => {
   return (
     <div className="flex flex-col gap-4 min-h-screen w-full bg-gray-950 text-amber-50">
-      <SearchBar/>
-      <Tabs/>
-      <ResultGrid/>
+      <SearchBar />
+      <Tabs />
+      <ResultGrid />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
